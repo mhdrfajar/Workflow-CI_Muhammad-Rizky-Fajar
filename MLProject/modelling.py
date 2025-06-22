b3950cb609ed51a687bd34a5917b77358813f25f
mhdrfajar <143498093+mhdrfajar@users.noreply.github.com>
2025-06-22 15:52:10 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import mlflow
 import os
+import sys
 import mlflow.sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
